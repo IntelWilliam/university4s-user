87724e0d7b75232a9ef8ffc664f17ab868b6c46a
Ubuntu <ubuntu@ip-172-31-18-248.ec2.internal>
2023-06-02 22:16:26 +0000

    update ibc

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ibceducacion.com',
+      script: '/home/ubuntu/akronenglish-user/dist/src/server/index.js',
+      env_production: {
+        PORT: '3017',
+        NODE_ENV: 'production',
+        NODE_PATH: '/home/ubuntu/akronenglish-user/dist',
+        MONGOLAB_URI: 'mongodb://cosmoEditor:tCmY49WBhcSQwyrs@44.206.92.190:15514/cosmo',
+        API_URL: 'https://php.ibceducacion.com/',
+        API_BASE_URL: 'https://php.ibceducacion.com/base/api/',
+        API_BASE_URL_CHAT: 'https://php.ibceducacion.com/base/ajax/',
+        AJAX_BASE_URL: 'https://php.ibceducacion.com/base/ajax/',
+        DOMAIN: 'ibceducacion.com',
+      },
+    },
+  ],
+};
