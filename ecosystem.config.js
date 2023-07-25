e2f0825c7d6299aec1ed6cf74bfff8cdb330bdf5
sangit <jordy@HRCPU00091>
2023-07-25 15:38:45 -0500

    configuracion de mongo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
         PORT: '3017',
         NODE_ENV: 'production',
         NODE_PATH: '/home/ubuntu/akronenglish-user/dist',
-        MONGOLAB_URI: 'mongodb://cosmoEditor:tCmY49WBhcSQwyrs@44.206.92.190:15514/cosmo',
+        MONGOLAB_URI: 'mongodb://cosmoEditor:tCmY49WBhcSQwyrs@54.145.189.241:15514/cosmo',
         API_URL: 'https://php.ibceducacion.com/',
         API_BASE_URL: 'https://php.ibceducacion.com/base/api/',
         API_BASE_URL_CHAT: 'https://php.ibceducacion.com/base/ajax/',
