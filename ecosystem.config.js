715d68918c586f15f915bc8769b2269fb9ac3f42
home-will <william.sangay@inteligenio.com>
2023-09-24 23:31:29 -0500

    ecosystem insituto

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: 'ibceducacion.com',
-      script: '/home/ubuntu/ibc-user/dist/src/server/index.js',
+      name: "ibcinstituto.com",
+      script: "/home/ubuntu/ibc-user/dist/src/server/index.js",
       env_production: {
-        PORT: '3017',
-        NODE_ENV: 'production',
-        NODE_PATH: '/home/ubuntu/ibc-user/dist',
-        MONGOLAB_URI: 'mongodb://cosmoEditor:tCmY49WBhcSQwyrs@54.145.189.241:15514/cosmo',
-        API_URL: 'https://ibceducacion.com/',
-        API_BASE_URL: 'https://ibceducacion.com/base/api/',
-        API_BASE_URL_CHAT: 'https://ibceducacion.com/base/ajax/',
-        AJAX_BASE_URL: 'https://ibceducacion.com/base/ajax/',
-        DOMAIN: 'ibceducacion.com',
+        PORT: "3017",
+        NODE_ENV: "production",
+        NODE_PATH: "/home/ubuntu/ibc-user/dist",
+        MONGOLAB_URI:
+          "mongodb://cosmoEditor:tCmY49WBhcSQwyrs@44.197.94.152:15514/cosmo",
+        API_URL: "https://php.ibcinstituto.com/",
+        API_BASE_URL: "https://php.ibcinstituto.com/base/api/",
+        API_BASE_URL_CHAT: "https://php.ibcinstituto.com/base/ajax/",
+        AJAX_BASE_URL: "https://php.ibcinstituto.com/base/ajax/",
+        DOMAIN: "ibcinstituto.com",
       },
     },
   ],
 };
 
-// probando estas urls y apis
+// cmabiando la configuracion para el insituto
