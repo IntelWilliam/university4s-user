e43320b03adaaa8ce3eebdbceb8d4a09e4567dfd
IntelWilliam <william.sangay@inteligenio.com>
2023-10-06 15:12:29 -0500

    regresando ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "ibcinstituto.com",
+      name: "b12.ibcinstituto.com",
       script: "/home/ubuntu/ibc-user/dist/src/server/index.js",
       env_production: {
         PORT: "3017",
@@ -9,14 +9,14 @@ module.exports = {
         NODE_PATH: "/home/ubuntu/ibc-user/dist",
         MONGOLAB_URI:
           "mongodb://cosmoEditor:tCmY49WBhcSQwyrs@44.197.94.152:15514/cosmo",
-        API_URL: "https://php.ibcinstituto.com/",
-        API_BASE_URL: "https://php.ibcinstituto.com/base/api/",
-        API_BASE_URL_CHAT: "https://php.ibcinstituto.com/base/ajax/",
-        AJAX_BASE_URL: "https://php.ibcinstituto.com/base/ajax/",
-        DOMAIN: "ibcinstituto.com",
+        API_URL: "https://phpb12.ibcinstituto.com/",
+        API_BASE_URL: "https://phpb12.ibcinstituto.com/base/api/",
+        API_BASE_URL_CHAT: "https://phpb12.ibcinstituto.com/base/ajax/",
+        AJAX_BASE_URL: "https://phpb12.ibcinstituto.com/base/ajax/",
+        DOMAIN: "b12.ibcinstituto.com",
       },
     },
   ],
-};
+}
 
 // cmabiando la configuracion para el insituto
