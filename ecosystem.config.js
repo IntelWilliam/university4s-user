e45135deef1e286f71b672ff2026dc7757a30a27
IntelWilliam <william.sangay@inteligenio.com>
2023-11-16 11:02:54 -0500

    ecosysteme para university4s

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: "b12.ibcinstituto.com",
+      name: "university4s.ibcidiomas.com",
       script: "/home/ubuntu/ibc-user/dist/src/server/index.js",
       env_production: {
         PORT: "3017",
         NODE_ENV: "production",
         NODE_PATH: "/home/ubuntu/ibc-user/dist",
         MONGOLAB_URI:
-          "mongodb://cosmoEditor:tCmY49WBhcSQwyrs@44.197.94.152:15514/cosmo",
-        API_URL: "https://phpb12.ibcinstituto.com/",
-        API_BASE_URL: "https://phpb12.ibcinstituto.com/base/api/",
-        API_BASE_URL_CHAT: "https://phpb12.ibcinstituto.com/base/ajax/",
-        AJAX_BASE_URL: "https://phpb12.ibcinstituto.com/base/ajax/",
-        DOMAIN: "b12.ibcinstituto.com",
+          "mongodb://cosmoEditor:tCmY49WBhcSQwyrs@3.208.146.92:15514/cosmo",
+        API_URL: "https://phpuniversity4s.ibcidiomas.com/",
+        API_BASE_URL: "https://phpuniversity4s.ibcidiomas.com/base/api/",
+        API_BASE_URL_CHAT: "https://phpuniversity4s.ibcidiomas.com/base/ajax/",
+        AJAX_BASE_URL: "https://phpuniversity4s.ibcidiomas.com/base/ajax/",
+        DOMAIN: "university4s.ibcidiomas.com",
       },
     },
   ],
